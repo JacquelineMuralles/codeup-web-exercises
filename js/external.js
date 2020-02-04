705f22b3f3d2e972d202a3b4557708c73bbe56eb
Jacqueline Muralles <jacquelinenmuralles@gmail.com>
2020-02-04 16:00:28 -0600

    created external and internal js exercises

---
+++ js/external.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my website!");
+
+// var userPrompt = prompt("What is your favorite color?");
+// alert ('Great ' + userPrompt + ' is my favorite color too!');
+
+// var littleMermaid = Number(prompt("How long would you like to rent The Little Mermaid for?"));
+// var brotherBear = Number(prompt("How long would you like to rent Brother Bear for?"));
+// var hercules = Number(prompt("How long would you like to rent Hercules for?"));
+// var rentalPrice = Number(prompt("How much is each rental per day?"));
+// var rentalTotal = Number((littleMermaid + brotherBear + hercules) * rentalPrice);
+// alert('Wonderful! Your total is $' + rentalTotal + ' .');
+
+// var gPay = Number(prompt("How much are you being paid by Google per hour?"));
+// var gHours = Number(prompt("How many hours did you work for Google this week?"));
+// var gTotal = Number(gPay * gHours);
+// var aPay = Number(prompt("How much are you being paid by Amazon per hour?"));
+// var aHours = Number(prompt("How many hours did you work for Amazon this week?"));
+// var aTotal = Number(aPay * aHours);
+// var fbPay = Number(prompt("How much are you being paid by Facebook per hour?"));
+// var fbHours = Number(prompt("How many hours did you work for Facebook this week?"));
+// var fbTotal = Number(fbPay * fbHours);
+// var totalPay = Number(gTotal + aTotal + fbTotal);
+// alert("Congratulations! Your total pay for the week is $" + totalPay + ".");
+
+var classFull = confirm("Is the class full?");
+var classSchedule = confirm("Does this class conflict with your current schedule?");
+// var canTakeClass =
+
